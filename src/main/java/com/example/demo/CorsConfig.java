60fda06f0815e096efaea5cc17bf972e0c9ea682
qali23 <aqureshi@softwareinstitute.com>
2024-02-27 09:54:35 +0000

    "new cors url origin + auto-find datasource"

--- src/main/java/com/example/demo/CorsConfig.java
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://34.238.40.69:3000") // Update with your React app's URL
+                .allowedOrigins("http://localhost:3000", "http://3.86.207.43:3000") // Update with your React app's URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
